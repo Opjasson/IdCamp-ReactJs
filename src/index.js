ba304c4982c5ba916ee92ee5b933bcd31b3b74bd
Opjasson <azmitheusos333@gmail.com>
2024-12-07 11:27:14 +0700

    add gitignore

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-
-const heading = React.createElement("div", null, "Biodata Perusahaan");
-
-const root = createRoot(document.getElementById("root"));
-
-root.render(heading);
