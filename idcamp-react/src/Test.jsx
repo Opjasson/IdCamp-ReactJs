9b1a7b0d4d564c152a726d9a43e185e190d6f4b5
Opjasson <azmitheusos333@gmail.com>
2024-12-07 13:29:40 +0700

    testing props

---
+++ idcamp-react/src/Test.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Test(props) {
+    const name = props.name;
+    const cita_cita = props.cita_cita;
+    return (
+        <div>
+            <h1>Hallo Nama Saya {name} cita-cita saya {cita_cita}</h1>
+        </div>
+    );
+}
+
+export default Test;
--- idcamp-react/src/index.js
+++ idcamp-react/src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Test from './Test';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <Test name="azmi ghazy asyrof" cita_cita="menjadi programer terkenal dan sholeh" />
+    <Test />
+    <Test />
   </React.StrictMode>
 );
 
